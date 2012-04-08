5f58cc19c7c0ab7299fec09dc1e959c588c0a002
Tom Powell <tompowell@gmail.com>
2012-04-08 12:23:42 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twiroku::Application.config.secret_token = 'f5eaa4459b5e477dd8014379c97efcee669cd20f51fe5adcb03507dcd8f657a68b092f5287a74ffa375522060dcd21f5cf597aae12e96193cdf858000f1ab66a'
